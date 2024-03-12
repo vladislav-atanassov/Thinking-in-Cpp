4829d715ab16fc9d77dbcfadb8a53faa33d4afa2
Vladislav Atanassov <vlavalat@gmail.com>
2024-03-12 23:13:24 +0200

    One more exercise

---
+++ Exercises/Chapter 3/ex_7.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void pointerStr(std::string* str)
+{
+    str->append(" passed as pointer");
+
+    std::cout << *str << std::endl;
+}
+
+void referenceStr(std::string& str)
+{
+    str.append(" then passed as reference");
+
+    std::cout << str << std::endl;
+}
+
+int main()
+{
+    std::string s = "apple";
+
+    std::cout << "Original string: " << s << std::endl;
+
+    pointerStr(&s);
+    referenceStr(s);
+
+    return 0;
+}
